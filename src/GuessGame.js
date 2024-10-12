6dec6e209ee7b40aad459ce46d8880e2c08fdbfe
boris <boris@gratchev.com>
2024-10-12 20:11:11 +0200

    form  submittable

--- src/GuessGame.js
+++ src/GuessGame.js
@@ -18,19 +18,21 @@ function GuessGame() {
 
     const sendGuess = async (guess) => {
         try {
-            
-            const response = await axios.post('http://localhost:8080/guess', { number: guess});
+
+            const response = await axios.post('http://localhost:8080/guess', { number: guess });
             // const response = await axios.get('http://localhost:8080/');
             console.log('Response:', response.data);
-            setGuessHistory([...guessHistory, {number: guess, response: response.data}]);
+            setGuessHistory([...guessHistory, { number: guess, response: response.data }]);
         } catch (error) {
             console.error('Error sending guess:', error);
         }
     };
 
-    const handleButtonClick = () => {
+    const handleStartSend = (event) => {
+        event.preventDefault();
         console.log('input value: ' + inputValue)
         sendGuess(Number(inputValue));
+        setInputValue('')
     };
 
     return (
@@ -47,12 +49,12 @@ function GuessGame() {
                 <div className="window-body">
                     <pre>Microsoft&#10094;R&#10095; Windows DOS
                         &#10094;C&#10095; Copyright Microsoft Corp 1990-2001.
-                        <br/>
+                        <br />
                         <br />Try to guess my favourite number
                         {guessHistory.map((guess, index) => (
                             <div key={index}>
-                                C:&#92;WINDOWS&#92;SYSTEM32{">"}{guess.number}
-                                <br/>{guess.response}
+                                C:&#92;WINDOWS&#92;SYSTEM64{">"}{guess.number}
+                                <br />{guess.response}
                             </div>
                         ))}
                     </pre>
@@ -63,8 +65,10 @@ function GuessGame() {
 
             <div className="field-row">
                 <label htmlFor="text1">Guess</label>
-                <input id="text1" type="text" value={inputValue} onChange={handleInputValue} />
-                <button onClick={handleButtonClick}>Send</button>
+                <form onSubmit={handleStartSend}>
+                    <input id="text1" type="text" value={inputValue} onChange={handleInputValue} />
+                </form>
+                <button onClick={handleStartSend}>Send</button>
             </div>
 
         </>
